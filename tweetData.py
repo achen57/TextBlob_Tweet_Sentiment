68efd7e2448a1e37b69b1ab5e878c1722a05d0ba
achen7296 <achen7296@gmail.com>
2020-09-07 12:45:29 -0400

    Bug fix: Allows users to continuously update tweet database without error

--- tweetData.py
+++
@@ -1,7 +0,0 @@
-class tweetData:
-    def __init__(self, username, tweet, polarity, sentiment):
-        self.username = username
-        self.tweet = tweet
-        self.polarity = polarity
-        self.sentiment = sentiment
-
